63206f1e0c01adb6ccd74d6a754e468de34fc362
Ran Gu <earlguwang@gmail.com>
2018-10-13 18:14:22 -0700

    Revert "Added course_info.json"
    This reverts commit f02def36218ef6e1246182f295ee4077664cb846.

--- src/components/CoursePane/CourseDetailPane.js
+++ src/components/CoursePane/CourseDetailPane.js
@@ -4,7 +4,6 @@ import {ArrowBack} from '@material-ui/icons';
 import SectionTable from "./SectionTable";
 import './sectiontable.css'
 import AlmanacGraphWrapped from "../AlmanacGraph/AlmanacGraph";
-import course_info from "./course_info.json";
 
 class CourseDetailPane extends Component {
     render() {
@@ -23,16 +22,10 @@ class CourseDetailPane extends Component {
                     <Typography variant='title' style={{flexGrow: '1', marginTop: 12}}>
                         {this.props.courseDetails.name[0] + ' ' + this.props.courseDetails.name[1]}
                     </Typography>
-
-                    <AlmanacGraphWrapped term = {this.props.term} courseDetails={this.props.courseDetails}/>
-
-                </div>
-
-                <div
-
-                >
-                </div>
-
+                </div> 
+                
+                <AlmanacGraphWrapped term = {this.props.term} courseDetails={this.props.courseDetails}/>
+                
                 <SectionTable style={{marginTop: 12}}
                               courseDetails={this.props.courseDetails}
                               onAddClass={this.props.onAddClass}/>
