33921688a7f4a972cf1a4208d1b65fdc24210c73
Ran Gu <earlguwang@gmail.com>
2019-01-25 16:36:22 -0800

    Added FB option to Paul Revere

--- src/components/CoursePane/popover_findspot.js
+++ src/components/CoursePane/popover_findspot.js
@@ -5,6 +5,9 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Popover from '@material-ui/core/Popover';
 import Input from '@material-ui/core/Input';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 
 const styles = theme => ({
   typography: {
@@ -15,7 +18,8 @@ const styles = theme => ({
 class SPopover extends React.Component {
   state = {
     anchorEl: null,
-    userEmail:""
+    userEmail: "",
+    userFB: ""
   };
 
 
@@ -27,9 +31,9 @@ class SPopover extends React.Component {
     if (typeof Storage !== "undefined") {
          email = window.localStorage.getItem("email");
       }
-      
+
       this.setState({ anchorEl: event.currentTarget,userEmail: email });
-    
+
   };
 
   handleClose = (event) => {
@@ -41,13 +45,13 @@ class SPopover extends React.Component {
     });
   };
 
-  getMeSpot = () =>{
+  signupEmail = () =>{
     const code = this.props.code;
     const email = this.state.userEmail;
     const name = this.props.name[1] + " " + this.props.name[2]
-   
 
-    let url = "https://mediaont.herokuapp.com/"
+
+    let url = "https://mediaont.herokuapp.com/email/"
 
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if(!re.test(email))
@@ -73,6 +77,26 @@ class SPopover extends React.Component {
     // fetch(url)
     // alert(email+" added to the notification list for "+ code +" !!!")
   }
+
+  signupFB = () =>{
+    const code = this.props.code;
+    const fb = this.state.userFB;
+    const name = this.props.name[1] + " " + this.props.name[2]
+
+
+    let url = "https://mediaont.herokuapp.com/facebook"
+
+    url = url + code +"/"+ name + "/" + fb;
+    window.localStorage.setItem("fb", fb);
+    this.setState({
+        anchorEl: null,
+      },()=>{
+        fetch(url)
+        this.props.handleSave(1,fb,code);
+      });
+  }
+
+
   inputChange = (event) =>{
     if (!event)  event = window.event;
     event.cancelBubble = true;
@@ -86,7 +110,7 @@ class SPopover extends React.Component {
 
     return (
       <React.Fragment>
-       
+
         <Button
           aria-owns={open ? 'simple-popper' : undefined}
           aria-haspopup="true"
@@ -114,27 +138,63 @@ class SPopover extends React.Component {
             horizontal: 'right',
           }}
         >
-          <Typography className={classes.typography}>Get notified when  a spot opens!</Typography>
-          
-          <div className={classes.container}>
-      
-         
+        <Typography className={classes.typography}>Get notified when section is no longer full!</Typography>
+
+        <div className={classes.container}>
+
+
           <Input
            style={{ margin: 10 }}
             onChange={this.inputChange}
             placeholder="Email"
             className={classes.input}
             defaultValue={this.state.userEmail}
-
             inputProps={{
               'aria-label': 'Description',
             }}
           />
-   
-          <Button variant="text" color="primary" className={classes.button} onClick={this.getMeSpot}>
-            Add</Button>
-           
+
+          <Button variant="text"
+                  color="primary"
+                  className={classes.button}
+                  onClick={this.signupEmail}>
+            Add
+          </Button>
+
         </div>
+
+        <div className={classes.container}>
+          <Input
+           style={{ margin: 10 }}
+            onChange={this.inputChange}
+            placeholder="Facebook ID**"
+            className={classes.input}
+            defaultValue={this.state.userFB}
+            inputProps={{
+              'aria-label': 'Description',
+            }}
+          />
+
+          <Button variant="text"
+                  color="primary"
+                  className={classes.button}
+                  onClick={this.signupFB}>
+            Add</Button>
+        </div>
+
+        <ExpansionPanel>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography className={classes.heading}>**Why? What Facebook ID?</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Sorry fam, but you're not unique. That's why getting your name won't help me find you on FB.
+              Instead, please enter the unique serial code with which FB has objectified you. Thanks!
+              Don't know what it is? <a href="https://findmyfbid.com" target="_blank">Find it here; no login requried.</a>
+              If this is your first time, please check Settings >> People >> Message Requests >> filtered requests in the Messenger App for a confirmation message. And then you're all set!
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
 
         </Popover>
       </React.Fragment>
